2014d9d9fc80329908e7be65f9abb4c4e8ce6afd
Christopher Kiessling <crsk@mail.com>
2018-09-29 20:06:45 -0300

    local connection fix

--- script.py
+++ script.py
@@ -1,37 +1,30 @@
 import config
+import mysql.connector
 import time
 
 mongodb = config.mongodb
-mongocol = mongodb["detalle_boleta"]
-
-def getLastLocalId():
-    mysqldb = config.mysqldb
-    mysqldb.execute("SELECT * FROM detalle_boleta")    
-    for row in mysqldb:
-        last_local_id = row[0]
-    return last_local_id
+mongocol = mongodb["boletas"]
 
 def getLastRemoteId():
+    last_remote_id = 0
     for row in mongocol.find({}, { "sync_id": 1, "_id": 0 }):
         last_remote_id = row['sync_id']
     return last_remote_id
 
 def uploadChanges():
     last_remote_id = getLastRemoteId()
-    mysqldb = config.mysqldb
-    mysqldb.execute("SELECT * FROM detalle_boleta")
-    for row in mysqldb:
+    conn = config.connect()
+    mysqldb = conn.cursor()
+    mysqldb.execute("SELECT * FROM boletas")
+    result_set = mysqldb.fetchall()
+    for row in result_set:
         local_id = row[0]
         if (int(local_id) > int(last_remote_id)):
             mongocol.insert_one({ "sync_id": local_id })
             print('record uploaded')
     print('Synced')
+    mysqldb.close()
 
 while True:
     time.sleep(5)
     uploadChanges()
-
-
-
-# record = [{ "sync_id": 2762 }, { "sync_id": 2763 }]
-# mongocol.insert_many(record)
